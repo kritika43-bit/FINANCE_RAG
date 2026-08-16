55b39c93e47f97f9fe190b20c94c1713e3d07cf7
kritika43-bit <prasadkritika43@gmail.com>
2026-08-16 10:46:45 +0530

    Remove unwated file

--- test_rag_storage.py
+++
@@ -1,8 +0,0 @@
-import ingest
-import rag
-
-
-def test_shared_collection_name():
-    assert hasattr(ingest, "COLLECTION_NAME")
-    assert hasattr(rag, "COLLECTION_NAME")
-    assert ingest.COLLECTION_NAME == rag.COLLECTION_NAME == "finance_rag"
